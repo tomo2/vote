aa6cf598f6684e15e4dd61fb8415fd957aff0239
tomo <rhbgp7kt@gmail.com>
2023-02-10 01:46:53 +0900

    ボタンの取得設定

--- main.js
+++ main.js
@@ -2,8 +2,15 @@
 
 {
 
+    const strawberry = document.getElementById('strawberry');
+    const orange = document.getElementById('orange');
+    const apple = document.getElementById('apple');
+    const grape = document.getElementById('grape');
+
+
     const button = document.querySelector('button');
     button.addEventListener('click', () => {
-        let 
+        let count = document.getElementById('count1');
+        count.textContent = button;
     });
 }
