2e4f081c52aeeaa5f623fe1172fe73b0faf55473
tomo <rhbgp7kt@gmail.com>
2023-02-11 00:11:09 +0900

    数字が出力されるように設定

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 'use strict';
 
 {
-
+    // ４つのボタンを取得
     const strawberry = document.getElementById('strawberry');
     const orange = document.getElementById('orange');
     const apple = document.getElementById('apple');
     const grape = document.getElementById('grape');
 
-
-    const button = document.querySelector('button');
-    button.addEventListener('click', () => {
-        let count = document.getElementById('count1');
-        count.textContent = button;
+    // ボタンを押すと数字が一つ増える
+    strawberry.addEventListener('click', () => {
+        let count1 = document.getElementById('count1');
+        count1.textContent = strawberry.value++;
     });
+    orange.addEventListener('click', () => {
+        let count2 = document.getElementById('count2');
+        count2.textContent = orange.value++;
+    });
+    apple.addEventListener('click', () => {
+        let count3 = document.getElementById('count3');
+        count3.textContent = apple.value++;
+    });
+    grape.addEventListener('click', () => {
+        let count4 = document.getElementById('count4');
+        count4.textContent = grape.value++;
+    });
+
 }
